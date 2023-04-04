913f3d811ad2891b8449c8299bc774dba39bcf1a
TomasGerling <104175925+TomasGerling@users.noreply.github.com>
2023-04-04 18:46:59 -0300

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = require('./src/app')
-const PORT = 8080
-app.set('port', PORT)
-
-app.listen(PORT, function (err) {
-    if (err) console.log(err);
-    console.log("Server listening on PORT", PORT);
-});
